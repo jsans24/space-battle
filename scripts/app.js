b2808e78a1dbf398ebfe1d7e7f2392535298d5fe
jsans24 <jsans24@gmail.com>
2020-09-17 13:40:14 -0700

    Commit 3 - Base Game Done

--- scripts/app.js
+++ scripts/app.js
@@ -2,6 +2,10 @@ function findRandomIntFromInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min);
 };
 
+function getRandomNum() {
+    return Math.random()
+}
+
 class Spaceship {
     constructor(name, hull, firePower, accuracy) {
         this.name = name;
@@ -11,14 +15,14 @@ class Spaceship {
     };
 
     attack(target){
-        if (Math.random() <= this.accuracy) {
+        if (getRandomNum() <= this.accuracy) {
             target.hull -= this.firePower;
             console.log(`${this.name}'s attack hit ${target.name}`)
         } else console.log(`${this.name}'s attack missed ${target.name}`)
     };
 };
 
-const USS_Assembly = new Spaceship('USS Assembly', 20, 5, .7)
+const yourShip = new Spaceship('USS Assembly', 20, 5, .7)
 
 class Alien_Ship extends Spaceship {
     constructor (name) {
@@ -26,17 +30,38 @@ class Alien_Ship extends Spaceship {
     };
 };
 
-const aShip1 = new Alien_Ship('Alien Ship 1');
+const attackingAliens = []
 
-for (i = 0; i < 10000; i++) {
-    if (i % 2 !== 0 && aShip1.hull > 0) {
-        USS_Assembly.attack(aShip1)
-        console.log(`Alien Ship: ${aShip1.hull} ------ USS Assembly: ${USS_Assembly.hull}`);
-    } else if (USS_Assembly.hull > 0 && aShip1.hull > 0) {
-        aShip1.attack(USS_Assembly)
-        console.log(`Alien Ship: ${aShip1.hull} ------ USS Assembly: ${USS_Assembly.hull}`);
-    } else if (USS_Assembly.hull <= 0) {
-        console.log('The USS Assembly has fallen. All hope is lost');
+for (k = 0; k < findRandomIntFromInt(6, 12); k++) attackingAliens[k] = new Alien_Ship(`Alien Ship ${k + 1}`)
+
+console.log(attackingAliens);
+
+for (j = 0; j < attackingAliens.length; j++) {
+    for (i = 1; i < attackingAliens.length * 10000; i++) {
+        if (attackingAliens[j] === attackingAliens[attackingAliens.length - 1] && attackingAliens[attackingAliens.length - 1].hull <=0) {
+            console.log('You have defeated the Alien Menace... For now...');
+            break;
+        } else if (attackingAliens[j].hull <= 0) {
+            break;
+        } else if (yourShip.hull <= 0) {
+            console.log('The USS Assembly has fallen. All hope is lost');
+            break;
+        } else if (i % 2 !== 0 && attackingAliens[j].hull > 0) {
+            yourShip.attack(attackingAliens[j])
+            console.log(`${attackingAliens[j].name}: ${attackingAliens[j].hull} ------ USS Assembly: ${yourShip.hull}`);
+        } else if (yourShip.hull > 0 && attackingAliens[j].hull > 0) {
+            attackingAliens[j].attack(yourShip)
+            console.log(`${attackingAliens[j].name}: ${attackingAliens[j].hull} ------ USS Assembly: ${yourShip.hull}`);
+        }
+    }
+    if (yourShip.hull <= 0 || attackingAliens[attackingAliens.length - 1].hull <= 0){
         break;
+    } else if (attackingAliens[j].hull <= 0) {
+        if (confirm('Retreat?')) {
+            console.log('What a coward!!!');
+            break;
+        }
     }
 }
+
+
